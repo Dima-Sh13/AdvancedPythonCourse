de1a1bfb7afcbb94db3e21526a5bb6076058bc05
Dima Shalupnya <dshalupnya@gmail.com>
2026-01-03 17:31:38 +0100

    first project wip

--- first_project/main.py
+++ first_project/main.py
@@ -1,2 +1,7 @@
-def main():
-    return "Miau!"
+from typing import List,Text
+
+def main(entero: int) -> List[Text]:
+    lista = []
+    for i in range(entero):
+        lista.append("Miau!")
+    return lista
--- first_project/test_main.py
+++ first_project/test_main.py
@@ -1,5 +1,8 @@
 from main import main
 
 
-def test_main():
-    assert main() == "Miau!"
+def test_main_4():
+    assert main(4) == ["Miau!","Miau!","Miau!","Miau!"]
+
+def test_main_5():
+    assert main(5) == ["Miau!","Miau!","Miau!","Miau!","Miau!"]
